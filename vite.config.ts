9d7ede8df28238972096c1e9d8fa3f93b310cf56
Sergio Villanueva <sivillanueva@pm.me>
2025-12-11 20:20:03 -0600

    Add GitHub Actions workflow for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: process.env.GITHUB_ACTIONS ? '/mapeo-verde/' : '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
